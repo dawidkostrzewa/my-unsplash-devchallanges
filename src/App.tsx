df45603d2b347f054761b6591e1776c7937f0d05
Dawid Kostrzewa <dawid.ko1997@gmail.com>
2020-12-11 21:39:35 +0100

    RWD fixes and create gallery component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import Gallery from 'Components/Gallery';
 import Header from 'Components/Header';
 import React from 'react';
 function App() {
     return (
-        <div className="App">
+        <>
             <Header />
-        </div>
+            <Gallery />
+        </>
     );
 }
 
