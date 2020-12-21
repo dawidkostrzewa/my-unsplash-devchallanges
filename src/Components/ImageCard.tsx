82d63ad7012e1895facbfc363a42d7a6ce51e179
Dawid Kostrzewa <dawid.ko1997@gmail.com>
2020-12-21 19:31:32 +0100

    Create mansory layout

--- src/Components/ImageCard.tsx
+++ src/Components/ImageCard.tsx
@@ -1,26 +1,13 @@
 import { IImageResponse } from 'Core/Interfaces';
-import { useEffect, useRef, useState } from 'react';
 
 interface IProps {
     image: IImageResponse;
 }
 
 export default function ImageCard({ image }: IProps) {
-    const imageRef = useRef<HTMLImageElement>();
-    const [spans, setSpans] = useState(0);
-    function setImgSpans() {
-        const height = imageRef.current.clientHeight;
-        const currentSpans = Math.ceil(height / 200);
-        setSpans(currentSpans);
-    }
-
-    useEffect(() => {
-        setImgSpans();
-    }, []);
-
     return (
-        <div style={{ gridRowEnd: `span ${spans}` }}>
-            <img ref={imageRef} src={image.urls.regular} alt={image.alt_description} />
+        <div className="single-image">
+            <img src={image.urls.regular} alt={image.alt_description} />
         </div>
     );
 }
